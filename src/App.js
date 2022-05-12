44d9cc2a61f711ca198d1775ff5bee346b85668c
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-05-12 11:30:29 -0700

    Added components and route to pokedex and my team page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Pokedex from "./Components/Pokedex";
+import MyTeam from "./Components/MyTeam";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Pokedex</Link>
+        <Link to="/myTeam">My Team</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Pokedex />} />
+          <Route path="/myteam" element={<MyTeam />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Pokedex.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Pokedex() {
+  return (
+    <div>
+      <h1>This is the homepage!</h1>
+      <p>Pokedex</p>
+    </div>
+  );
+}
+
+export default Pokedex;
