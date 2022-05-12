0290a564bd49375bd4afbf9500250ae780a260c1
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-05-12 12:29:26 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// anytime a component wraps around another component...its called
-// as Higher Order Component
 ReactDOM.render(
   <Router>
     <App />
