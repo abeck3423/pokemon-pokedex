3c56faec9776a482851e7791c24b763a2a178829
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-05-12 16:18:42 -0700

    more updates

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -3,19 +3,23 @@ import PokemonDetails from "./PokemonDetails";
 import { useEffect, useState } from "react";
 
 function Pokemon(props) {
-    let [pokemon, setPokemon] = useState({});
+  let [pokemon, setPokemon] = useState({});
 
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${props.name}`)
-        .then((response) => response.json())
-        .then((data) => setPokemon(data) )
-    }, [pokemon]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.name}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }, [pokemon]);
 
-    return (
-        <div>
-            {pokemon.name ? <PokemonDetails pokemon={pokemon} /> : <h2>Loading...</h2>}
-        </div>
-    );
+  return (
+    <div>
+      {pokemon.name ? (
+        <PokemonDetails pokemon={pokemon} />
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
 
 export default Pokemon;
